81f7dd5ebad44353bc6ad128e806d8e0285d91a0
Mehdi Bounouif <mbounoui@c1r6p7.1337.ma>
2025-11-12 15:20:53 +0100

    create Fixed class

--- CPP-Module-02/ex00/Fixed.h
+++ CPP-Module-02/ex00/Fixed.h
@@ -6,8 +6,21 @@
 /*   By: mbounoui <mbounoui@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/12 14:36:33 by mbounoui          #+#    #+#             */
-/*   Updated: 2025/11/12 14:36:38 by mbounoui         ###   ########.fr       */
+/*   Updated: 2025/11/12 15:20:24 by mbounoui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef _MEHDI_
+#define _MEHDI_
 
+class Fixed
+{
+	int	fixed_point;
+	static const int fractionl;
+	public:
+		Fixed() { fixed_point = 0; }
+		int getRawBits(void) const { return (fixed_point); }
+		void setRawBits(int const raw) { this->fixed_point = raw; }
+};
+
+#endif
--- CPP-Module-02/ex00/main.cpp
+++ CPP-Module-02/ex00/main.cpp
@@ -6,8 +6,21 @@
 /*   By: mbounoui <mbounoui@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/12 14:36:29 by mbounoui          #+#    #+#             */
-/*   Updated: 2025/11/12 14:36:37 by mbounoui         ###   ########.fr       */
+/*   Updated: 2025/11/12 14:38:04 by mbounoui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include "Fixed.h"
 
+int main( void )
+{
+	Fixed a;
+	Fixed b( a );
+	Fixed c;
+	c = b;
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+	return 0;
+}
